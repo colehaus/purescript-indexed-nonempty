e0e4c5c8bf230bc311d35660615e63733f323dce
Cole Haus <colehaus@cryptolab.net>
2018-04-08 19:17:13 -0700

    Add additional convenience functions
    Also, reformat with `hindent`

--- src/Data/NonEmpty/Indexed.purs
+++ src/Data/NonEmpty/Indexed.purs
@@ -9,6 +9,7 @@ import Data.FoldableWithIndex
 import Data.FunctorWithIndex (class FunctorWithIndex, mapWithIndex)
 import Data.Generic (class Generic, gShow)
 import Data.Maybe (Maybe)
+import Data.NonEmpty as UnIndexed
 import Data.Ord (class Ord1, compare1)
 import Data.Semigroup.Foldable (class Foldable1, fold1Default)
 import Data.Traversable (class Traversable, traverse, sequence)
@@ -21,15 +22,21 @@ data NonEmpty f k v = NonEmpty (Tuple k v) (f k v)
 
 infixr 5 NonEmpty as :|
 
-foldl1 :: forall f k v. Foldable (f k) => (v -> v -> v) -> NonEmpty f k v -> v
+foldl1 ::
+     forall f k v. Foldable (f k)
+  => (v -> v -> v)
+  -> NonEmpty f k v
+  -> v
 foldl1 f ((Tuple _ v) :| fv) = foldl f v fv
 
 fromNonEmpty :: forall f k v r. (k -> v -> f k v -> r) -> NonEmpty f k v -> r
 fromNonEmpty f (Tuple k v :| fa) = f k v fa
 
 nonEmpty ::
-  forall f k v. (f k v -> Maybe (Tuple (Tuple k v) (f k v))) -> f k v ->
-  Maybe (NonEmpty f k v)
+     forall f k v.
+     (f k v -> Maybe (Tuple (Tuple k v) (f k v)))
+  -> f k v
+  -> Maybe (NonEmpty f k v)
 nonEmpty split = map (uncurry NonEmpty) <<< split
 
 head :: forall f k v. NonEmpty f k v -> Tuple k v
@@ -37,6 +44,47 @@ head (h :| _) = h
 
 tail :: forall f k v. NonEmpty f k v -> f k v
 tail (_ :| t) = t
+
+hmap ::
+     forall f g k.
+     (f k ~> g k)
+  -> NonEmpty f k
+  ~> NonEmpty g k
+hmap f (h :| t) = h :| f t
+
+deindex ::
+     forall f g k v u.
+     (Tuple k v -> u)
+  -> (f k v -> g u)
+  -> NonEmpty f k v
+  -> UnIndexed.NonEmpty g u
+deindex f g (h :| t) = f h UnIndexed.:| g t
+
+reindex ::
+     forall j k f v.
+     (k -> j)
+  -> (f k v -> f j v)
+  -> NonEmpty f k v
+  -> NonEmpty f j v
+reindex f g (Tuple k v :| t) = Tuple (f k) v :| g t
+
+index ::
+     forall f g u k v.
+     (u -> Tuple k v)
+  -> (g u -> f k v)
+  -> UnIndexed.NonEmpty g u
+  -> NonEmpty f k v
+index f g (h UnIndexed.:| t) = f h :| g t
+
+foldMapWithIndex1 ::
+     forall f k v s.
+     FoldableWithIndex k (f k)
+  => Semigroup s
+  => (k -> v -> s)
+  -> NonEmpty f k v
+  -> s
+foldMapWithIndex1 f ((Tuple k v) :| fkv) =
+  foldlWithIndex (\i s a -> s <> f i a) (f k v) fkv
 
 derive instance genericNonEmpty ::
   (Generic (f k v), Generic k, Generic v) =>
